0fe1857f726a072494d5ec0eb224af47102e6390
ldanielgiuliani <ldanielgiuliani@gmail.com>
2022-08-30 11:35:19 +0800

    Add preload and image handlers

--- index.js
+++ index.js
@@ -1,78 +1,144 @@
 const allowList = {
-	styles: [ /block-library\/style\.css/ ],
-	scripts: []
+    styles: [/block-library\/style\.css/],
+    scripts: [],
 }
 
-addEventListener( 'fetch', event => {
-	event.respondWith( handleRequest( event.request ) )
+const preloadList = {
+    styles: [],
+    scripts: [],
+    images: [],
+};
+
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
 });
 
 async function handleRequest(request) {
-	const url = new URL( request.url );
+    const url = new URL(request.url);
 
-	if ( url.pathname === '/robots.txt' ) {
-		return new Response(
-			'User-agent: *\nDisallow: /',
-			{
-				status: 200,
-				headers: {
-					'Content-Type': 'text/plain'
-				}
-			}
-		);
-	}
-	
-	const originalHost = request.headers.get( 'x-host' );
+    if (url.pathname === '/robots.txt') {
+        return new Response(
+            'User-agent: *\nDisallow: /',
+            {
+                status: 200,
+                headers: {
+                    'Content-Type': 'text/plain'
+                }
+            }
+        );
+    }
 
-	if ( ! originalHost ) {
-		return new Response(
-			JSON.stringify({
-				error: 'No host provided'
-			}),
-			{
-				status: 403,
-				headers: {
-					'Content-Type': 'application/json'
-				}
-			}
-		);
-	}
-	
-	url.hostname = originalHost;
+    const originalHost = request.headers.get('x-host');
 
-	const bypassTransform = Boolean( request.headers.get('x-bypass-transform') );
+    if (!originalHost) {
+        return new Response(
+            JSON.stringify({
+                error: 'No host provided'
+            }),
+            {
+                status: 403,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+    }
 
-	if ( ! bypassTransform  ) {
-		const response = await fetch( url.toString(), request );
+    url.hostname = originalHost;
 
-		return new HTMLRewriter()
-			.on( 'link[rel=stylesheet]', new StylesheetElementHandler() )
-			.on( 'script[src]', new ScriptElementHandler() )
-			.transform( response );
-	}
+    const bypassTransform = Boolean(request.headers.get('x-bypass-transform'));
 
-	return fetch( url.toString(), request );
+    if (!bypassTransform) {
+        const response = await fetch(url.toString(), request);
+
+        return new HTMLRewriter()
+            .on('link[rel=stylesheet]', new StylesheetElementHandler())
+            .on('script[src]', new ScriptElementHandler())
+            .on('img', new ImageElementHandler())
+            .on('header.wp-block-template-part .wp-block-image > img', new ImageLcpElementHandler())
+            .on('head', new HeadElementHandler())
+            .transform(response);
+    }
+
+    return fetch(url.toString(), request);
 }
 
 class StylesheetElementHandler {
-	element( linkElement ) {
-		const href = linkElement.getAttribute('href');
-		const bypass = allowList.styles.some( pattern => pattern.test( href ) );
+    element(linkElement) {
+        const href = linkElement.getAttribute('href');
+        const bypass = allowList.styles.some(pattern => pattern.test(href));
 
-		if ( ! bypass ) {
-			linkElement.setAttribute( 'media', 'print' );
-			linkElement.setAttribute( 'onload', 'this.media=\'all\'' );
-		}
-	}
+        if (!bypass) {
+            linkElement.setAttribute('media', 'print');
+            linkElement.setAttribute('onload', 'this.media=\'all\'');
+        } else {
+            if (!preloadList.styles.includes(href)) {
+                preloadList.styles.push(href);
+            }
+        }
+    }
 }
 
 class ScriptElementHandler {
-	element( scriptElement ) {
-		const src = scriptElement.getAttribute('src');
-		const bypass = allowList.scripts.some( pattern => pattern.test( src ) );
+    element(scriptElement) {
+        const src = scriptElement.getAttribute('src');
+        const bypass = allowList.scripts.some(pattern => pattern.test(src));
 
-		if ( ! bypass ) {
-			scriptElement.setAttribute( 'async', '' );
-		}
-	}
+        if (!bypass) {
+            scriptElement.setAttribute('async', '');
+        } else {
+            if (!preloadList.scripts.includes(src)) {
+                preloadList.scripts.push(src);
+            }
+        }
+    }
+}
+
+class ImageElementHandler {
+    element(imageElement) {
+        imageElement.setAttribute('decoding', 'async');
+        imageElement.setAttribute('loading', 'lazy');
+    }
+}
+
+class ImageLcpElementHandler {
+    element(ImageLcpElement) {
+        const src = ImageLcpElement.getAttribute('src');
+        const srcset = ImageLcpElement.getAttribute('srcset');
+        const sizes = ImageLcpElement.getAttribute('sizes');
+
+        if (!preloadList.images.some(pattern => pattern.href === src)) {
+            preloadList.images.push({
+                'href': src,
+                'imagesrcset': srcset,
+                'imagesizes': sizes
+            });
+        }
+
+        ImageLcpElement.removeAttribute('loading');
+        ImageLcpElement.setAttribute('fetchpriority', 'high');
+    }
+}
+
+class HeadElementHandler {
+    element(headElement) {
+        preloadList.styles.map((href) => {
+            headElement.prepend(`<link rel="preload" href="${href}" as="style">`, {html: true});
+        });
+        preloadList.scripts.map((href) => {
+            headElement.prepend(`<link rel="preload" href="${href}" as="script">`, {html: true});
+        });
+        preloadList.images.map((e) => {
+            const attr = [
+                e.href ? `href="${e.href}"` : '',
+                e.imagesrcset ? `imagesrcset="${e.imagesrcset}"` : '',
+                e.imagesizes ? `imagesrcset="${e.imagesizes}"` : ''
+            ];
+            const htmlAttr = attr.filter(function (el) {
+                return el !== '';
+            }).join(' ');
+
+            headElement.prepend(`<link fetchpriority="high" rel="preload" as="image" ${htmlAttr}>`, {html: true});
+        });
+    }
 }
